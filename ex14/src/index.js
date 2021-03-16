12ea3d08cb2ca1e3a01588e07ad9ae6202a45b33
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-16 23:06:11 +0100

    Add files via upload

--- ex14/src/index.js
+++ ex14/src/index.js
@@ -1,1 +1,19 @@
+// Only change code below this line
+function myForLoop1 () {
+    for (var i=0; i<9; i++) {
+        console.log(i);
+    }
+    return evenNumbers;
+};
+function myForLoop2 () {
+    for (var i=0; i<9, i++) {
+        console.log(i);
+    }
+    returnInverseNumbers;
+}
+// Only change code above this line
 
+module.exports = {
+    myForLoop1,
+    myForLoop2
+};
