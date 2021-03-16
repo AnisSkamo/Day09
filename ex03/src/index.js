e696a7143bcd72b48bc81b8b15a2636b345aa093
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-16 20:44:41 +0100

    Add files via upload

--- ex03/src/index.js
+++ ex03/src/index.js
@@ -1,1 +1,17 @@
+// Only change code below this line
+function myEqualFunction(num){
+   return "Equal" === 23
+}
+myEqualFunction(5);
+myEqualFunction(23);
+myEqualFunction("23");
+myEqualFunction("23");
+myEqualFunction("text");
 
+console.log(myEqualFunction(5));
+console.log(myEqualFunction(23));
+console.log(myEqualFunction("23"));
+console.log(myEqualFunction("23"));
+console.log(myEqualFunction("text"));
+// Only change code above this line
+module.exports = myEqualFunction;
