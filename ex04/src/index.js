d780cbdefc747c6adf56309a8680cb7c72ce8a66
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-16 20:56:43 +0100

    Add files via upload

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,1 +1,14 @@
-
+// Only change code below this line
+function compareDifferentValues(m,n){
+    if (m === n)
+    return "Equal"
+    else
+    return "Not Equal"
+};
+console.log(compareDifferentValues(8, "8"));
+console.log(compareDifferentValues("8", 8));
+console.log(compareDifferentValues('8', 8));
+console.log(compareDifferentValues("8", "8"));
+console.log(compareDifferentValues(8, 8));
+// Only change code above this line
+module.exports = compareDifferentValues;
