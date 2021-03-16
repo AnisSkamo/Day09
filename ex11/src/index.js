942a2b4ccc7606f5d16a4ff34a4fae33f2c1ee60
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-16 22:22:59 +0100

    Add files via upload

--- ex11/src/index.js
+++ ex11/src/index.js
@@ -1,1 +1,26 @@
-
+function switchCase(letter) {
+    //Write code below this line
+   var animals()
+    switch (letter) {
+        case "a":
+        "antelope";
+        break;
+        case "b":
+        "bird";
+        break;
+        case "c":
+        "cat";
+        break;
+        case "default":
+        "stuff";
+        break;
+       } 
+    
+    //Write code above this line
+    return animals;
+}
+console.log(switchCase("a"));
+console.log(switchCase("b"))
+console.log(switchCase("c"))
+console.log(switchCase("default"))
+module.exports = switchCase;
