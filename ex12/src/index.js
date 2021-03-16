3e28330e4d70915e26cc1632c56ea33ca08f291b
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-16 22:38:20 +0100

    Add files via upload

--- ex12/src/index.js
+++ ex12/src/index.js
@@ -1,1 +1,11 @@
+function switchCaseSequential (num) {
+    // Write code below this line
+var answer = num;
+    num(1,3)
 
+
+    // Write code above this line
+    return answer;
+}
+console.log(switchCaseSequential()); // Change this line
+module.exports = switchCaseSequential;
