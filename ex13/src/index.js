d458ba5d5d19f64ce1bcf076a64298c5f2fc9a75
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-16 22:51:14 +0100

    Add files via upload

--- ex13/src/index.js
+++ ex13/src/index.js
@@ -1,1 +1,12 @@
-
+// Only change code below this line
+function inverseWhile() {
+        while(inverseWhile < 5) {
+        console.log(inverseWhile);
+        inverseWhile++;
+        return fiveNumbers;
+    }
+    
+}
+// Only change code above this line
+console.log(inverseWhile());
+module.exports = inverseWhile;
